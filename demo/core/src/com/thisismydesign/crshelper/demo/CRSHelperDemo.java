defbcfad264e21fac2daa7f442883d262267ef7e
csaba <csaba.apagyi@gmail.com>
2016-08-28 19:19:30 +0200

    Add demo dependency to crshelper jar (because cannot add project dependency); create simple demo

--- demo/core/src/com/thisismydesign/crshelper/demo/CRSHelperDemo.java
+++ demo/core/src/com/thisismydesign/crshelper/demo/CRSHelperDemo.java
@@ -2,32 +2,44 @@ package com.thisismydesign.crshelper.demo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.thisismydesign.crshelper.screen.Frame;
+import com.thisismydesign.crshelper.shape.spline.Spline;
 
 public class CRSHelperDemo extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
+	Spline spline;
+	private ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
+
+		shapeRenderer = new ShapeRenderer();
+
+		int screenHeight = Gdx.graphics.getHeight();
+		int screenWidth = Gdx.graphics.getWidth();
+		Vector2[] cps = new Frame(10, screenWidth, screenHeight).getRandomPoints();
+
+		spline = new Spline(Color.CYAN, cps, cps);
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		spline.render(shapeRenderer);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
