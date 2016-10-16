664edf1eaab6bad3004cb580073bbef6aa41714b
csaba <csaba.apagyi@gmail.com>
2016-10-16 22:18:19 +0200

    Clean demo

--- demo/core/src/com/thisismydesign/crshelper/demo/CRSHelperDemo.java
+++ demo/core/src/com/thisismydesign/crshelper/demo/CRSHelperDemo.java
@@ -10,11 +10,10 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.thisismydesign.crshelper.iterator.Precision;
 import com.thisismydesign.crshelper.screen.Frame;
+import com.thisismydesign.crshelper.shape.Intersectable;
 import com.thisismydesign.crshelper.shape.Line;
 import com.thisismydesign.crshelper.shape.spline.Spline;
 import com.thisismydesign.crshelper.shape.spline.SplineAnimator;
-
-import java.util.List;
 
 public class CRSHelperDemo extends ApplicationAdapter {
 	private SpriteBatch batch;
@@ -28,10 +27,8 @@ public class CRSHelperDemo extends ApplicationAdapter {
 	DemoSpline upperSpline;
 	SplineAnimator upperSplineAnimator;
 
-	Line upperMiddleLine;
-	Line lowerMiddleLine;
-	Line verticalMiddleLine;
 	Line diagonalMiddleLine;
+	Intersectable diagonalMiddleIntersectable;
 
 	final int sampleCount = 100;
 	
@@ -46,13 +43,11 @@ public class CRSHelperDemo extends ApplicationAdapter {
 		precision = new Precision(1f);
 		final int maxPointRangeInPercent = 100;
 
-		System.out.println(precision.getAllowedErrorInPixels());
-
 		Vector2 upperFrameStartPoint = new Vector2(0f, 0f);
 		Vector2 lowerFrameStartPoint = new Vector2(0f, screenHeight/2);
 
-		Vector2[] upperCPs1 = new Frame(10, screenWidth, screenHeight, upperFrameStartPoint).getRandomPoints(maxPointRangeInPercent);
-		Vector2[] upperCPs2 = new Frame(10, screenWidth, screenHeight, upperFrameStartPoint).getRandomPoints(maxPointRangeInPercent);
+		Vector2[] upperCPs1 = new Frame(10, screenWidth, screenHeight/2, upperFrameStartPoint).getRandomPoints(maxPointRangeInPercent);
+		Vector2[] upperCPs2 = new Frame(10, screenWidth, screenHeight/2, upperFrameStartPoint).getRandomPoints(maxPointRangeInPercent);
 		lowerSpline = new DemoXFlatSpline(upperCPs1, precision);
 		lowerSplineAnimator = new SplineAnimator(lowerSpline, upperCPs2);
 
@@ -60,10 +55,8 @@ public class CRSHelperDemo extends ApplicationAdapter {
 		upperSpline.move(lowerFrameStartPoint);
 		upperSplineAnimator = new SplineAnimator(upperSpline, new Spline(upperCPs2, precision).move(lowerFrameStartPoint).getControlPoints());
 
-		upperMiddleLine = new Line(new Vector2(screenWidth/2, screenHeight/2), new Vector2(screenWidth/2, screenHeight), 2);
-		lowerMiddleLine = new Line(new Vector2(screenWidth/2, screenHeight/2), new Vector2(screenWidth/2, 0), 2);
-		verticalMiddleLine = new Line(new Vector2(screenWidth /2, screenHeight /2), new Vector2(screenWidth /2, screenHeight /2), 2);
 		diagonalMiddleLine = new Line(new Vector2(0f,0f), new Vector2(screenWidth, screenHeight), 2);
+		diagonalMiddleIntersectable = diagonalMiddleLine;
 	}
 
 	@Override
@@ -71,24 +64,22 @@ public class CRSHelperDemo extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		upperSpline.dumbRender(shapeRenderer);
+		upperSplineAnimator.animate(0.001f);
+
 		lowerSpline.render(shapeRenderer, Color.CYAN);
 		lowerSpline.renderDebug(shapeRenderer);
 		lowerSplineAnimator.animate(0.001f);
 
-		upperSpline.dumbRender(shapeRenderer);
-		upperSplineAnimator.animate(0.001f);
-
 		diagonalMiddleLine.render(shapeRenderer, Color.MAGENTA);
 
-		List<Vector2> intersections = lowerSpline.intersect(diagonalMiddleLine);
-		for (Vector2 intersection : intersections) {
+		for (Vector2 intersection : lowerSpline.intersect(diagonalMiddleLine)) {
 			shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 			shapeRenderer.circle(intersection.x, intersection.y, 5);
 			shapeRenderer.end();
 		}
 
-		intersections = upperSpline.intersect(diagonalMiddleLine);
-		for (Vector2 intersection : intersections) {
+		for (Vector2 intersection : upperSpline.intersect(diagonalMiddleIntersectable)) {
 			shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 			shapeRenderer.circle(intersection.x, intersection.y, 5);
 			shapeRenderer.end();
