f7793a38bab50e1e91aab8c65d47ecafa6e26ed1
csaba <csaba.apagyi@gmail.com>
2016-10-17 23:38:20 +0200

    Refactor Span class

--- crshelper/src/main/java/com/thisismydesign/crshelper/shape/spline/Span.java
+++ crshelper/src/main/java/com/thisismydesign/crshelper/shape/spline/Span.java
@@ -13,6 +13,7 @@ public class Span {
     private final static int numberOfPoints = 4;
     private Cubic equation;
     public CatmullRomSpline<Vector2> spline;
+    private final static double alpha = 0.5;
 
     public Span(Vector2[] controlPoints) {
         if (controlPoints.length != numberOfPoints) throw new IllegalArgumentException(String.format(
@@ -25,10 +26,10 @@ public class Span {
     public List<Vector2> intersect(Line line) {
         List<Vector2> intersections = new ArrayList<>();
 
-        double c0 = line.getA()*getA0(controlPoints) + line.getB()*getB0(controlPoints) + line.getC();
-        double c1 = line.getA()*getA1(controlPoints) + line.getB()*getB1(controlPoints);
-        double c2 = line.getA()*getA2(controlPoints) + line.getB()*getB2(controlPoints);
-        double c3 = line.getA()*getA3(controlPoints) + line.getB()*getB3(controlPoints);
+        double c3 = line.getA()*getA1(controlPoints) + line.getB()*getA2(controlPoints);
+        double c2 = line.getA()*getB1(controlPoints) + line.getB()*getB2(controlPoints);
+        double c1 = line.getA()*getC1(controlPoints) + line.getB()*getC2(controlPoints);
+        double c0 = line.getA()*getD1(controlPoints) + line.getB()*getD2(controlPoints) + line.getC();
 
         equation.solve(c3, c2, c1, c0);
 
@@ -45,31 +46,31 @@ public class Span {
         return intersections;
     }
 
-    private double getA0(Vector2[] controlPoints) {
-        return 0.5*2*controlPoints[1].x;
-    }
-    private double getB0(Vector2[] controlPoints) {
-        return 0.5*2*controlPoints[1].y;
-    }
-
     private double getA1(Vector2[] controlPoints) {
-        return 0.5*(-1*controlPoints[0].x + controlPoints[2].x);
+        return alpha*(-1*controlPoints[0].x + 3*controlPoints[1].x - 3*controlPoints[2].x + controlPoints[3].x);
     }
-    private double getB1(Vector2[] controlPoints) {
-        return 0.5*(-1*controlPoints[0].y + controlPoints[2].y);
+    private double getA2(Vector2[] controlPoints) {
+        return alpha*(-1*controlPoints[0].y + 3*controlPoints[1].y - 3*controlPoints[2].y + controlPoints[3].y);
     }
 
-    private double getA2(Vector2[] controlPoints) {
-        return 0.5*(2*controlPoints[0].x - 5*controlPoints[1].x + 4*controlPoints[2].x - controlPoints[3].x);
+    private double getB1(Vector2[] controlPoints) {
+        return alpha*(2*controlPoints[0].x - 5*controlPoints[1].x + 4*controlPoints[2].x - controlPoints[3].x);
     }
     private double getB2(Vector2[] controlPoints) {
-        return 0.5*(2*controlPoints[0].y - 5*controlPoints[1].y + 4*controlPoints[2].y - controlPoints[3].y);
+        return alpha*(2*controlPoints[0].y - 5*controlPoints[1].y + 4*controlPoints[2].y - controlPoints[3].y);
     }
 
-    private double getA3(Vector2[] controlPoints) {
-        return 0.5*(-1*controlPoints[0].x + 3*controlPoints[1].x - 3*controlPoints[2].x + controlPoints[3].x);
+    private double getC1(Vector2[] controlPoints) {
+        return alpha*(-1*controlPoints[0].x + controlPoints[2].x);
     }
-    private double getB3(Vector2[] controlPoints) {
-        return 0.5*(-1*controlPoints[0].y + 3*controlPoints[1].y - 3*controlPoints[2].y + controlPoints[3].y);
+    private double getC2(Vector2[] controlPoints) {
+        return alpha*(-1*controlPoints[0].y + controlPoints[2].y);
+    }
+
+    private double getD1(Vector2[] controlPoints) {
+        return alpha*2*controlPoints[1].x;
+    }
+    private double getD2(Vector2[] controlPoints) {
+        return alpha*2*controlPoints[1].y;
     }
 }
